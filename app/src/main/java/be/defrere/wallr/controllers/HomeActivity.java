47836baec23662d2524ed868a16604a8edb9b196
Jonas <defrere.jonas@gmail.com>
2017-11-22 22:01:55 +0100

    :art: Adding menu items to toolbar

--- app/src/main/java/be/defrere/wallr/controllers/HomeActivity.java
+++ app/src/main/java/be/defrere/wallr/controllers/HomeActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
 import be.defrere.wallr.R;
 
 public class HomeActivity extends AppCompatActivity {
@@ -27,6 +30,37 @@ public class HomeActivity extends AppCompatActivity {
 
         // Set toolbar
         this.setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings: {
+
+                // TODO add settings view
+
+                break;
+            }
+            case R.id.menu_credentials: {
+
+                // TODO add credentials view
+
+                break;
+            }
+            case R.id.menu_about: {
+
+                // TODO add about view
+
+                break;
+            }
+        }
+        return false;
     }
 
     public void onAddClick(View v) {
